5c7ef92d0e2fea663dd383c5fe537e92c3133f3c
amirshariati <seyed.amir.shariati@gmail.com>
2023-11-05 15:56:05 +0330

    create pages

---
+++ src/pages/home/index.js
@@ -0,0 +1,41 @@
+import {Grid, Paper,} from "@material-ui/core";
+import {homeStyles} from "./style";
+import {CryptoPortfolioTracker} from "../../components/CryptoPortfolioTracker/cryptoPortfolioTracker";
+
+
+
+
+
+
+
+const Home = (props) => {
+
+    const classes = homeStyles();
+
+
+
+    return (
+        <>
+            <div style={{marginTop:150 ,marginLeft:80,marginRight:80}}>
+
+
+                <Grid container >
+                    <Grid item xs={1} md={1} lg={1} xl={1} />
+
+                    <Grid item xs={10} md={10} lg={10} xl={10}>
+                        <Paper classname={classes.homeBg} >
+                            <CryptoPortfolioTracker />
+                        </Paper>
+                    </Grid>
+
+                    <Grid item xs={1} md={1} lg={1} xl={1} />
+
+                </Grid>
+
+            </div>
+
+        </>
+    )
+}
+
+export {Home}
---
+++ src/pages/home/style.js
@@ -0,0 +1,79 @@
+import {makeStyles} from "@material-ui/core";
+// import bg from '../../assets/img/back.webp'
+
+const homeStyles = makeStyles((theme) => ({
+    root: {
+        // maxHeight: '100%',
+        display: 'flex',
+        width: '100%',
+        backgroundColor:'#253344',
+
+    },
+    homeBg:{
+        backgroundColor:'#253344',
+    },
+    boxCard: {
+        borderRadius: 20,
+        backgroundColor: '#d7d7d7',
+        width:300,
+        height:300,
+        marginRight: theme.spacing(2),
+        marginLeft: theme.spacing(2),
+    },
+    boxPaper:{
+        display: 'flex',
+        backgroundColor: '#e3e3e3',
+        paddingRight: '10%',
+        margin:10
+    },
+    img: {
+        marginRight:50,
+        marginLeft:50,
+        marginTop:20
+    },
+    imgRemoveBack: {
+        marginRight:50,
+        marginLeft:50,
+        marginTop:2
+    },
+    title: {
+        fontFamily:'vazir',
+        fontSize:25,
+        fontWeight:500,
+        marginRight:'35%',
+        marginLeft:'30%',
+        textShadow:'3px 3px #fff'
+    } ,
+    title2: {
+        fontFamily:'vazir',
+        fontSize:25,
+        fontWeight:500,
+        marginRight:'15%',
+        marginLeft:'10%',
+        textShadow:'3px 3px #fff'
+    },
+    title3: {
+        fontFamily:'vazir',
+        fontSize:25,
+        fontWeight:500,
+        marginRight:'12%',
+        marginLeft:'10%',
+        textShadow:'3px 3px #fff'
+    },
+    title4: {
+        fontFamily:'vazir',
+        fontSize:25,
+        fontWeight:500,
+        marginRight:'5%',
+        marginLeft:'5%',
+        textShadow:'3px 3px #fff'
+    },
+    paperCard:{
+        backgroundColor:'#253344',
+        height: 300,
+        width:'100%',
+        marginTop:80,
+    }
+}))
+
+export {homeStyles}
