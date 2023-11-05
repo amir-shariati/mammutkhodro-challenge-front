0ceedfb640913c3f0c5f2a632a0bedb24293c4b7
amirshariati <seyed.amir.shariati@gmail.com>
2023-11-05 17:07:45 +0330

    create actions

---
+++ src/actions/index.js
@@ -0,0 +1,39 @@
+export {
+    cleanCategoryData,
+    fetchCategoryData
+} from './categoryAction'
+
+export {
+    cleanProductListData,
+    fetchProductListData
+} from './productListAction'
+
+export {
+    cleanApplicationListData,
+    fetchApplicationListData
+} from './applicationAction'
+
+export {
+    cleanBrandListData,
+    fetchBrandListData
+} from './brandAction'
+
+export {
+    cleanColorListData,
+    fetchColorListData
+} from './colorAction'
+
+export {
+    cleanElectricEnginListData,
+    fetchElectricEnginListData
+} from './electricEnginAction'
+
+export {
+    cleanFilterPanelData,
+    fetchFilterPanelData
+} from './filterPanelAction'
+
+export {
+    cleanGasolineEnginListData,
+    fetchGasolineEnginListData
+} from './gasolineEnginAction'
