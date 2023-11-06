7184b6ba27e25215a5b927bde716b21c4394eadf
amirshariati <seyed.amir.shariati@gmail.com>
2023-11-06 05:26:27 +0330

    add server urls

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,13 @@
 export {
-
+    url_stock_name,
+    url_stock_symbol,
+    url_stock_action,
+    url_stock_item_add,
+    url_stock_item_buy,
+    url_stock_item_sell,
+    url_stock_history,
+    url_portfolio_list,
+    url_stock_list,
+    url_stock_item_list,
+    url_transaction_list,
 } from './serverAPI'
--- src/config/serverAPI/index.js
+++ src/config/serverAPI/index.js
@@ -1,2 +1,16 @@
 import {SERVER_ADDRESS} from '../serverConfig'
 
+export const url_stock_symbol = `http://${SERVER_ADDRESS}/api/stock-symbol/`
+export const url_stock_name = `http://${SERVER_ADDRESS}/api/stock-name/`
+export const url_stock_action = `http://${SERVER_ADDRESS}/api/stock-action/`
+
+export const url_stock_item_add = `http://${SERVER_ADDRESS}/api/stock-item/add/`
+export const url_stock_item_buy = `http://${SERVER_ADDRESS}/api/stock-item/buy/`
+export const url_stock_item_sell = `http://${SERVER_ADDRESS}/api/stock-item/sell/`
+
+export const url_stock_history = `http://${SERVER_ADDRESS}/api/stock/history/`
+
+export const url_portfolio_list = `http://${SERVER_ADDRESS}/api/portfolio/`
+export const url_stock_list = `http://${SERVER_ADDRESS}/api/stock/`
+export const url_stock_item_list = `http://${SERVER_ADDRESS}/api/stock-item/`
+export const url_transaction_list = `http://${SERVER_ADDRESS}/api/transaction/`
