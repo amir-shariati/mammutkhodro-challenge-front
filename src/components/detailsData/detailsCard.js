315f7072f31092090c3450665fc1aeac8d1b92ad
amirshariati <seyed.amir.shariati@gmail.com>
2023-11-12 23:25:33 +0330

    add portfolio balance

--- src/components/detailsData/detailsCard.js
+++ src/components/detailsData/detailsCard.js
@@ -17,7 +17,7 @@ export const DetailsCard =()=>{
                 <Grid item xs={12} md={12} lg={12} xl={12}>
                    <Typography className={classes.coinTitle}>Holdings (2 coin)</Typography>
                     <Divider style={{backgroundColor:'#323E50',height:0.5}}/>
-                    <Typography className={classes.text}>Holdings</Typography>
+                    <Typography className={classes.text}>Balance <span>{portfolio.balance}</span></Typography>
                     <Divider style={{height:0.2 ,marginRight:'5%',marginLeft:'5%',backgroundColor:'#212E3D'}}/>
                     <Typography className={classes.text}>Holdings</Typography>
                 </Grid>
